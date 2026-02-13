153de7134169a3aacb43df3cac1bd0c016339a94
inayat-khan910 <inayat@studentdiwan.com>
2026-02-13 10:08:28 +0530

    some minor changes

--- Routes/category.routes.js
+++ Routes/category.routes.js
@@ -9,7 +9,7 @@ import {
 const categoryRouter = Router();
 
 categoryRouter.post("/add", addCategory);
-categoryRouter.get("/", getCategories);
+categoryRouter.get("/getcatagories", getCategories);
 categoryRouter.put("/update/:id", updateCategory);
 categoryRouter.delete("/delete/:id", deleteCategory);
 
