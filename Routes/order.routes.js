2a8f290a6fa83f1357350a961f9640b9c6584831
inayat-khan910 <inayat@studentdiwan.com>
2026-02-13 10:12:22 +0530

    some minor update

--- Routes/order.routes.js
+++ Routes/order.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createOrder, getOrdersbyuserId ,getOrderDetail} from "../Controllers/orders.controller.js";
+import { createOrder, getOrdersbyuserId, getOrderDetail } from "../Controllers/orders.controller.js";
 
 const orderRouter = Router();
 
 orderRouter.post("/create", createOrder);
-orderRouter .get("/:userId", getOrdersbyuserId);
-orderRouter .get("/:orderId", getOrderDetail);
+orderRouter.get("/getordersbyuser/:userId", getOrdersbyuserId);
+orderRouter.get("/getorderdetails/:orderId", getOrderDetail);
 
 
-export default orderRouter ;
+export default orderRouter;
